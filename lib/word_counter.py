9029a1f17f0d7ae90f202bf03295d903f5305bd6
Ania <sz.ania.04@gmail.com>
2019-01-12 18:49:06 +0100

    refactor word_counter

--- lib/word_counter.py
+++ lib/word_counter.py
@@ -3,9 +3,7 @@ import re
 
 
 def word_counter(s):
-    cnt = Counter()
     string_string = " ".join(s)
-    for word in string_string.split():
-        re_word = re.sub(r'[^\w\s]', '', word)
-        cnt[re_word] += 1
+    re_word = re.sub(r'[^\w\s]', '', string_string)
+    cnt = Counter(re_word.split())
     return cnt
