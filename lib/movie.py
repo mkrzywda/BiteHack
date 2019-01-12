6d608ed683eb1e7367e0b3be5e817f1a58f5495d
Ania <sz.ania.04@gmail.com>
2019-01-12 19:28:07 +0100

    added arguments to Movie class

--- lib/movie.py
+++ lib/movie.py
@@ -8,8 +8,12 @@ class Movie(NamedTuple):
     # rating: float
     keywords: List[str]
     genres: List[str]
+    quotes: List[str]
+    scenario: List[str]
+    direction: List[str]
 
 
 if __name__ == '__main__':
-    movie = Movie(title="ASdas", year=1234, actors=["asd ads"], keywords=["a", "b"], genres=["a", "b"])
+    movie = Movie(title="ASdas", year=1234, actors=["asd ads"], keywords=["a", "b"], genres=["a", "b"], \
+                  quates=["ab", "bc"], scenario = ["a b"], direction=["c d"])
     print(movie)
