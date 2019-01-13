11f6f734d5e6604ef0383627b9b068ffe7a24847
Ania <sz.ania.04@gmail.com>
2019-01-13 03:47:41 +0100

    refactor word_counter and main

--- lib/word_counter.py
+++ lib/word_counter.py
@@ -1,11 +1,16 @@
 from collections import Counter
-import re
 from nltk.stem import PorterStemmer
 import nltk.data
+import nltk
+from nltk.corpus import stopwords
 #nltk.download()
 
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 porter_stemmer = PorterStemmer()
+STOPWORDS = set(stopwords.words('english'))
+STOPWORDS.add('I')
+for i in set(stopwords.words('english')):
+    STOPWORDS.add(porter_stemmer.stem(i))
 
 
 def word_counter(subtitles):
@@ -13,4 +18,8 @@ def word_counter(subtitles):
     for i in cnt.copy():
         if i[0].islower() and porter_stemmer.stem(i) != i:
             cnt[porter_stemmer.stem(i)] += cnt[i]
+
+    for i in STOPWORDS:
+        del cnt[i]
+    print(cnt.keys())
     return cnt
