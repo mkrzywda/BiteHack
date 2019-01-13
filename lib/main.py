31ea12bf504e7e6f24010be3b9458cff9631ffc3
Ania <sz.ania.04@gmail.com>
2019-01-13 05:18:50 +0100

    refactor main

--- lib/main.py
+++ lib/main.py
@@ -11,9 +11,13 @@ def main(path):
     print(path)
     x = normalization.normalize_srt(path)
     z = word_counter.word_counter(x)
-    most_z = [word for word, _ in z.most_common(50)]
+    most_no = 50
+    if len(z) > most_no:
+        most_z = [word for word, _ in z.most_common(most_no)]
+    else:
+        most_z = list(z.keys())
     y = binding_words_vector.load_vectors(VECTOR_PATH)
-    cluster=plot_words.cluster_analysis(most_z)
+    cluster = plot_words.cluster_analysis(most_z)
 
 if __name__ == '__main__':
     shrek_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'Shrek (2001) DVDRiP KvCD Hockney(TUS Release).en.srt')
