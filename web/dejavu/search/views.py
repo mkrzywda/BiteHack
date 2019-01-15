791a8e5fc931f156d76097cb8c96bf179938e2c0
Paweł Kubiak <pawel.kubiak.it@gmail.com>
2019-01-15 01:37:36 +0100

    Handle elasticsearch query parsing errors

--- web/dejavu/search/views.py
+++ web/dejavu/search/views.py
@@ -13,27 +13,30 @@ def index(request):
     }
 
     if query:
-        res = es.search(index="movies", body={
-            "query": {
-                "query_string" : {
-                    # "default_field" : "text",
-                    "fields": ["title^5", "text", "keywords^5", "actors^2", "scenario^2", "genres", "direction^2"],
-                    "query" : query
-                }
-            }})
-
         results = []
-        for hit in res['hits']['hits']:
-            # title = hit['_source']['title']
-            # del hit['_source']
-            result = hit['_source']
-            result['score'] = round(hit['_score'], 2)
-            result['size'] = min(200, 40*result['score'])
-            if(result['score'] < 0.5):
-                continue
-            results.append(result)
-            # results.append((hit['_score'], title))
 
+        try:
+            res = es.search(index="movies", body={
+                "query": {
+                    "query_string" : {
+                        # "default_field" : "text",
+                        "fields": ["title^5", "text", "keywords^5", "actors^2", "scenario^2", "genres", "direction^2"],
+                        "query" : query
+                    }
+                }})
+
+            for hit in res['hits']['hits']:
+                # title = hit['_source']['title']
+                # del hit['_source']
+                result = hit['_source']
+                result['score'] = round(hit['_score'], 2)
+                result['size'] = min(200, 40*result['score'])
+                if(result['score'] < 0.5):
+                    continue
+                results.append(result)
+                # results.append((hit['_score'], title))
+        except Exception:
+            pass
         context['results'] = results
 
     return render(request, 'search/index.html', context)
